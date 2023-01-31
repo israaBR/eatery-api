f804501c14356235f30454ba0a4a1b954c79b582
Israa <48530021+israaBR@users.noreply.github.com>
2023-02-01 01:45:07 +0200

    add routers usage

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 //TODO import routes
+const MenuRouter = require("./routes/menu");
+const CustomersRouter = require("./routes/customers");
+const CartsRouter = require("./routes/carts");
 
 //TODO declare app & DB
 const APP = express();
@@ -21,5 +24,6 @@ mongoose.connect(MONGO_URL, (error) => {
 
 //TODO use routers
 APP.use(express.json());
-// APP.use("/menu", MenuRouter);
-// APP.use("/customer", CustomersRouter);
+APP.use("/menu", MenuRouter);
+APP.use("/customer", CustomersRouter);
+APP.use("/cart", CartsRouter);
